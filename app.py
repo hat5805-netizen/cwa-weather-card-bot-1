26c9ff29abfcb94dabf68ce13ef6334e94067549
hat5805-netizen <hat5805@gmail.com>
2025-11-07 13:52:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,21 +22,11 @@ def run_script():
 def home():
     return "CWA Weather Card Bot is running!"
 
-#@app.route('/run')
-#def run():
-#    thread = threading.Thread(target=run_script)
-#    thread.start()
-#    return "任務已啟動！正在下載天氣小卡... (請查看 Render Logs 確認結果)"
-
-@app.route("/run")
-def run_task():
-    try:
-        from download_cwa3day_card import parse_weather_ad_card
-        parse_weather_ad_card()
-        return "OK", 200  # ✅ 避免輸出太多文字
-    except Exception as e:
-        return "ERR", 200  # ✅ 一樣不要輸出太大內容
-
+@app.route('/run')
+def run():
+    thread = threading.Thread(target=run_script)
+    thread.start()
+    return "任務已啟動！正在下載天氣小卡... (請查看 Render Logs 確認結果)"
 
 if __name__ == '__main__':
     import os
